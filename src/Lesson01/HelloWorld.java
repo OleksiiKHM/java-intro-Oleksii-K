8238b04e7b9bfbaf4910b3ead59dea133c04de93
Oleksii_K <personalemail@gmail.com>
2018-03-13 19:25:24 +0200

    Initial commit

---
+++ src/Lesson01/HelloWorld.java
@@ -0,0 +1,22 @@
+package Lesson01;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+        boolean bool = true;
+        int i = 2;
+        long veryLong = 50000000000L;
+        float fl = 9.5F;
+        double d = 2.7;
+        char t = 't';
+
+        System.out.println(bool);
+        System.out.println(i);
+        System.out.println(veryLong);
+        System.out.println(fl);
+        System.out.println(d);
+        System.out.println(t);
+
+    }
+}
